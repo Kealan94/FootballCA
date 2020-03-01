608187cfbe937ea4ecbfc13a661b49c0f3a21236
Kealan Crilly <crilly2k12@hotmail.co.uk>
2020-03-01 13:25:47 +0000

    sass made

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
-import App      from './components/App';
-<link rel="stylesheet" href="styles/main.css"></link>
+import App      from './components/App';s
 
 ReactDOM.render(<App />, document.getElementById('root'));
