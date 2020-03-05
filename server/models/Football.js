abf0ab8c1a09a864bcb9d7d37c8e764ee5dee6ef
Kealan Crilly <D00224427@student.dkit.ie>
2020-03-05 15:01:57 +0000

    added things to models

--- server/models/Football.js
+++ server/models/Football.js
@@ -9,8 +9,29 @@ const FootballerSchema  = new mongoose.Schema({
       required  : true,
       validator : value => !validator.isEmpty(value)
     
-    }
- 
+    },
+    
+   name: {
+      type      : String,
+      required  : true,
+      validator : value => !validator.isEmpty(value)
+    
+    },
+    
+   age: {
+      type      : String,
+      required  : true,
+      validator : value => !validator.isEmpty(value)
+    
+    },
+
+    
+    club: {
+      type      : String,
+      required  : true,
+      validator : value => !validator.isEmpty(value)
+    
+    },
   }, SchemeConfig);
 
   module.exports.Footballer = mongoose.model('Footballer', FootballerSchema);
